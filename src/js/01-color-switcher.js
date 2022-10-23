c528191af1cd15a17a7bb9f847423aeaa5ab17c8
AngelikaGralewska <angelika.gralewska@gmail.com>
2022-10-23 16:49:07 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+buttonStart.addEventListener('click', clickButtonStart);
+buttonStop.addEventListener('click', clickButtonStop);
+
+
+function clickButtonStart() {
+    timerId = setInterval(() => {
+         document.body.style.background = getRandomHexColor();
+     }, 1000);
+ };
+ 
+ function clickButtonStop() {
+     clearInterval(timerId);
+ };
+
 
